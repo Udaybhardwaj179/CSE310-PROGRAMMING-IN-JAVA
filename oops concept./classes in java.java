b9734f0176dcdb323d099fbc969d4ff0791d4a7a
uday bhardwaj <103742193+Udaybhardwaj179@users.noreply.github.com>
2023-01-19 16:02:30 +0530

    Create classes in java.java

---
+++ oops concept./classes in java.java
@@ -0,0 +1,19 @@
+class Demo{
+  int a;
+  int b;
+  float c;
+  static int d;// static variables and class variables
+  Demo obj; // refrence variable
+  //new Demo(); // this is created a object in java 
+  Demo obj=new Demo(); // now new object is created by new object and this object address is stored in reference variable
+  Demo{
+    // this is a constructor.
+  }
+  void m1(){
+    // this is a method in java or we can say finction.
+  }
+  static void m2(){
+    // this is the static method in java.
+  }
+}
+  
