b5955186e3d58e23ec720ff82e0b175a0f85617a
uday bhardwaj <103742193+Udaybhardwaj179@users.noreply.github.com>
2023-01-19 16:28:42 +0530

    Create creating class and seprate class of object.java

---
+++ oops concept./creating class and seprate class of object.java
@@ -0,0 +1,11 @@
+class f1{
+    void call (){
+        System.out.print("hello");
+    }
+}
+public class f2{
+    public static void main(String s[]){
+        f1 obj=new f1();
+        obj.call();
+    }
+}
