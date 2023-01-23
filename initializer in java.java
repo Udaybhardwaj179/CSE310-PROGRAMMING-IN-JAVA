3b40e8456f74b5dc35ae240444a78b4daa0e589e
uday bhardwaj <103742193+Udaybhardwaj179@users.noreply.github.com>
2023-01-23 16:45:18 +0530

    Create initializer in java.java

---
+++ initializer in java.java
@@ -0,0 +1,41 @@
+class Demo1{
+static{
+System.out.println("4");
+}
+Demo1(){
+System.out.println("1");
+}
+Demo1(int c){
+System.out.println("2");
+}
+void m1(){
+System.out.println("3");
+}
+static void m2(){
+System.out.println("5");
+}
+static{
+System.out.println("6");
+}
+  
+  {
+    this is the initialzer.
+    this statement is copied in all the constructors.
+  }
+  {
+    this is the second initializer and this statement is copied in all the constructors
+      .
+  }
+}
+class Demo{
+static{
+System.out.println("Main-static");
+}
+public static void main(String arv[]){
+Demo1 obj = new Demo1();
+Demo1.m2();
+obj.m1();
+Demo1 obj2 = new Demo1(10);
+obj2.m1();
+}
+}
